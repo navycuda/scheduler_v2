38ffd5ac4406c8836d21e694c6a050d9a5b691af
NavyCuda <navysupra@gmail.com>
2022-10-04 14:46:20 -0700

    Add InterviewerList

---
+++ src/components/InterviewerList/Item.js
@@ -0,0 +1,12 @@
+// src/components/InterviewerList/Item.js
+import React from 'react';
+
+const Item = (props) => {
+
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default Item;
---
+++ src/components/InterviewerList/index.js
@@ -0,0 +1,12 @@
+// src/components/InterviewerList/index.js
+import React from 'react';
+
+const InterviewerList = (props) => {
+
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default InterviewerList;
