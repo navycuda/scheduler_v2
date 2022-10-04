2c102ecb47331f6777f5863ff14381e1db177ffb
NavyCuda <navysupra@gmail.com>
2022-10-04 00:05:26 -0700

    Document DayList

--- src/components/DayList/index.js
+++ src/components/DayList/index.js
@@ -3,7 +3,19 @@ import React from 'react';
 
 import Item from './Item';
 
-const DayList = (props) => {
+/**
+ * 
+ * @param {Object} props
+ * @property {String} day - the currently selected Day
+ * @property {Array.<{id: Number, name: String, spots: Number}>} days - Array of day objects
+ * @property {Function} setDay - sets the currectly selected day and accepts the name of the day
+ * @returns react component
+ */
+const DayList = ({
+  day,
+  days,
+  setDay
+  }) => {
 
   return (
     <div>
