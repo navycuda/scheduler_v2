9b93473230d0840c22bba37618b1d55b72c99f4d
NavyCuda <navysupra@gmail.com>
2022-10-03 22:46:59 -0700

    Copy paste boilerplate, adjust to my code

--- stories/index.js
+++ stories/index.js
@@ -6,6 +6,8 @@ import { action } from "@storybook/addon-actions";
 import "index.scss";
 
 import Button from "components/Button";
+import DayList from "components/DayList";
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -21,4 +23,15 @@ storiesOf("Button", module)
     <Button disabled onClick={action("button-clicked")}>
       Disabled
     </Button>
+  ));
+
+storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  }) // Provides the default background color for our component
+  .add("Unselected", () => <DayList.Item name="Monday" spots={5} />) // To define our stories, we call add() once for each of our test states to generate a story
+  .add("Selected", () => <DayList.Item name="Monday" spots={5} selected />) 
+  .add("Full", () => <DayList.Item name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayList.Item name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   ));
