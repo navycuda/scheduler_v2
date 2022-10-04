d5718aa5fb4102b74733c5da4d1b6160ddceba18
NavyCuda <navysupra@gmail.com>
2022-10-04 14:54:13 -0700

    Document and Build initial interviewer item

--- src/components/InterviewerList/Item.js
+++ src/components/InterviewerList/Item.js
@@ -1,11 +1,27 @@
 // src/components/InterviewerList/Item.js
 import React from 'react';
 
+
+/**
+ * 
+ * @param {Object} props
+ * @property {Number} id - the id of the interviewer
+ * @property {String} name - the name of the interviewer
+ * @property {String} avatar - url of the interviewer avatar 
+ * @returns React Component
+ */
 const Item = (props) => {
 
   return (
-    <div>
-    </div>
+    <li
+      className="interviewers__item"
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+    </li>
   );
 };
 
