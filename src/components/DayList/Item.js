1b75330ebf57f583ef8e1c8104517e67551b5274
NavyCuda <navysupra@gmail.com>
2022-10-03 22:36:34 -0700

    Start the DayList & Item components

---
+++ src/components/DayList/Item.js
@@ -0,0 +1,27 @@
+// src/components/DayList/DayListItem.js
+import React from 'react';
+
+/**
+ * 
+ * @param {*} props
+ * @property {String} name - name of the day
+ * @property {Number} sports - the number of spots remaining
+ * @property {Boolean} selected - is this day selected
+ * @property {Function} setDay - accepts the name of the day
+ * @returns react component
+ */
+const Item = ({
+  name,
+  spots,
+  selected,
+  setDay
+}) => {
+
+  return (
+    <li>
+
+    </li>
+  );
+};
+
+export default Item;
---
+++ src/components/DayList/index.js
@@ -0,0 +1,16 @@
+// src/components/DayList/index.js
+import React from 'react';
+
+import Item from './Item';
+
+const DayList = (props) => {
+
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default Object.assign(DayList, {
+  Item
+});
