5ab9eb019eb76457bdd2a0bbfa69a51f78700f47
NavyCuda <navysupra@gmail.com>
2022-10-05 16:15:43 -0700

    Refactor value to name to get things to work

--- src/components/DayList/index.js
+++ src/components/DayList/index.js
@@ -11,12 +11,13 @@ import Item from './Item';
  * @param     {Function}  props.onChange - sets the currectly selected day and accepts the name of the day
  */
 const DayList = (props) => {
+  console.log(props);
   // Build array of items for the DayList
   const dayList = props.days.map((d) => {
     return (
       <Item
         key={d.id}
-        name={d.value}
+        name={d.name}
         spots={d.spots}
         selected={d.name === props.value}
         setDay={props.onChange}
