be59b54212bf142518d8bb16ab611ceb01584b6a
NavyCuda <navysupra@gmail.com>
2022-10-05 00:38:01 -0700

    Add prevent default

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -32,7 +32,10 @@ const Form = (props) => {
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form 
+          autoComplete="off"
+          onSubmit={(e) => e.preventDefault()}
+          >
           <input
             className="appointment__create-input text--semi-bold"
             name={student}
