e62fccbbb0adb06b796b9606f530a9a1b8c3f470
NavyCuda <navysupra@gmail.com>
2022-10-04 21:13:28 -0700

    Improve documentation, refactor to props

--- src/components/DayList/index.js
+++ src/components/DayList/index.js
@@ -6,25 +6,21 @@ import Item from './Item';
 /**
  * 
  * @param {Object} props
- * @property {String} day - the currently selected Day
- * @property {Array.<{id: Number, name: String, spots: Number}>} days - Array of day objects
- * @property {Function} setDay - sets the currectly selected day and accepts the name of the day
+ * @param {String} props.value - the currently selected Day
+ * @param {Array.<{id: Number, name: String, spots: Number}>} props.days - Array of day objects
+ * @param {Function} props.onChange - sets the currectly selected day and accepts the name of the day
  * @returns react component
  */
-const DayList = ({
-  day,
-  days,
-  setDay
-}) => {
+const DayList = (props) => {
 
-  const dayList = days.map((d) => {
+  const dayList = props.days.map((d) => {
     return (
       <Item
         key={d.id}
         name={d.name}
         spots={d.spots}
-        selected={d.name === day}
-        setDay={setDay}
+        selected={d.name === props.value}
+        setDay={props.onChange}
       />
     );
   });
