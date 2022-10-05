26bf3772c21b1c5ce40bd13e5b118ada2ceaf357
NavyCuda <navysupra@gmail.com>
2022-10-05 00:02:40 -0700

    Build status component

--- src/components/Appointment/Status.js
+++ src/components/Appointment/Status.js
@@ -1,11 +1,23 @@
 // src/components/Appointment/Status.js
 import React from 'react';
 
+/**
+ * 
+ * @param {Object} props
+ * @param {String} props.message 
+ * @returns 
+ */
 const Status = (props) => {
 
   return (
-    <div>
-    </div>
+    <main className="appointment__card appointment__card--status">
+      <img
+        className="appointment__status-image"
+        src="images/status.png"
+        alt="Loading"
+      />
+      <h1 className="text--semi-bold">{props.message}</h1>
+    </main>
   );
 };
 
