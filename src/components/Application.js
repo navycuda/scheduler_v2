8f63e739845cea10270c88dcfbaab181c5972890
NavyCuda <navysupra@gmail.com>
2022-10-05 14:22:58 -0700

    Prep, remove mock data for axios

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,24 +5,6 @@ import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 const appointments = {
   "1": {
     id: 1,
