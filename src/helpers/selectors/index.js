64087d4377ce03a11576899d2a0807dc98658696
NavyCuda <navysupra@gmail.com>
2022-10-06 14:56:33 -0700

    Setup exports

--- src/helpers/selectors/index.js
+++ src/helpers/selectors/index.js
@@ -1,17 +1,2 @@
 // src/helpers/selectors/index.js
-import React from 'react';
-
-/**
- *
- * @param     {Object}    props
- */
-
-const selectors = (props) => {
-
-  return (
-    <>
-    </>
-  );
-};
-
-export default selectors;
+export {default as getAppointmentsForDay} from './getAppointmentsForDay.js';
