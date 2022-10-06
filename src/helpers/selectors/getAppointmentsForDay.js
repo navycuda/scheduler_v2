ee12209e5b0df644102ed833c9659d24a82ab4f8
NavyCuda <navysupra@gmail.com>
2022-10-06 14:55:51 -0700

    Setup test run of function

--- src/helpers/selectors/getAppointmentsForDay.js
+++ src/helpers/selectors/getAppointmentsForDay.js
@@ -1,17 +1,7 @@
 // src/helpers/selectors/getAppointmentsForDay.js
-import React from 'react';
-
-/**
- *
- * @param     {Object}    props
- */
-
 const getAppointmentsForDay = (state, day) => {
-
-  return (
-    <>
-    </>
-  );
+  console.log('getAppointmentsForDay', { state, day });
+  return [];
 };
 
 export default getAppointmentsForDay;
