504b0eff829b45ac376c77df9f0a90702676bd97
NavyCuda <navysupra@gmail.com>
2022-10-06 14:11:57 -0700

    Start building helpers

---
+++ src/helpers/selectors/index.js
@@ -0,0 +1,17 @@
+// src/helpers/selectors/index.js
+import React from 'react';
+
+/**
+ *
+ * @param     {Object}    props
+ */
+
+const selectors = (props) => {
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default selectors;
