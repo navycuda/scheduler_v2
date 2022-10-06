14693fe437b57556b01296a65279f118364b912a
NavyCuda <navysupra@gmail.com>
2022-10-06 14:40:28 -0700

    Setup more parts

---
+++ src/helpers/selectors/getAppointmentsForDay.js
@@ -0,0 +1,17 @@
+// src/helpers/selectors/getAppointmentsForDay.js
+import React from 'react';
+
+/**
+ *
+ * @param     {Object}    props
+ */
+
+const getAppointmentsForDay = (state, day) => {
+
+  return (
+    <>
+    </>
+  );
+};
+
+export default getAppointmentsForDay;
