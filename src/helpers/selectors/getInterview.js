95f4fa9f15239cc348f06d63562ad1c46b768cd8
NavyCuda <navysupra@gmail.com>
2022-10-07 13:37:07 -0700

    Initial build of getInterview

---
+++ src/helpers/selectors/getInterview.js
@@ -0,0 +1,22 @@
+// src/helpers/selectors/getInterview.js
+// @ts-check
+import React from 'react';
+
+/**
+ * Gets interview data if
+ * @param     {State}     state - current state of the app
+ * @param     {Interview} interview - interview object to 
+ * @returns   {null|Interview} 
+ */
+const getInterview = (state, interview) => {
+  if (!interview) return null;
+
+  const interviewer = state.interviewers[interview.interviewer.id];
+
+  return {
+    student: interview.student,
+    interviewer
+  };
+};
+
+export default getInterview;
