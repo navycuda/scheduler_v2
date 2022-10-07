8cee2ad66cc796f17322a49174c3f6742e18b5e5
NavyCuda <navysupra@gmail.com>
2022-10-06 21:55:20 -0700

    Document

--- src/helpers/selectors/getAppointmentsByIds.js
+++ src/helpers/selectors/getAppointmentsByIds.js
@@ -1,4 +1,12 @@
 // src/helpers/selectors/getAppointmentsByIds.js
+// @ts-check
+
+/**
+ * Gets the appointments by their id from the provided 
+ * @param {Appointments} appointments The appointment database to read
+ * @param {Array.<Number>} ids - Id numbers of the appointments to extract 
+ * @returns {Appointments}
+ */
 const getAppointmentsByIds = (appointments, ids) => {
   return ids.map((id) => {
     return appointments[id];
