fa7ea9a9e8020adbae4218f9de2adab55753908d
NavyCuda <navysupra@gmail.com>
2022-10-06 22:23:44 -0700

    Fix format issue

--- src/components/DayList/DayList.jsdoc.js
+++ src/components/DayList/DayList.jsdoc.js
@@ -4,10 +4,10 @@
  * DayList module
  * @module DayList
  */
- /**
-  * The day object used in the appointments
-  * @typedef    {Object}    Day
-  * @property   {Number}    id - Id number of the day
-  * @property   {String}    name - The name of the day, ie. "Monday"
-  * @property   {Number}    spots - The number of appointment spots remaining
-  */
+/**
+ * The day object used in the appointments
+ * @typedef    {Object}    Day
+ * @property   {Number}    id - Id number of the day
+ * @property   {String}    name - The name of the day, ie. "Monday"
+ * @property   {Number}    spots - The number of appointment spots remaining
+ */
