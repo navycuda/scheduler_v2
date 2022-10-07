6313d06aa2c63450072c183209cb6b53acff6f20
NavyCuda <navysupra@gmail.com>
2022-10-06 23:56:51 -0700

    Refactor and simplify getAppointmentsForDay

--- src/helpers/selectors/getAppointmentsForDay.js
+++ src/helpers/selectors/getAppointmentsForDay.js
@@ -6,13 +6,13 @@ import getAppointmentsByIds from "./getAppointmentsByIds";
 /**
  * Gets the Appoints for a day
  * @param     {Object}        state - Appointment state from which to query 
+ * @param     {String}        state.day - The selected day
  * @param     {Array.<Day>}   state.days - Array of day objects
  * @param     {Appointments}  state.appointments
- * @param     {String}        day 
  * @returns   {Array|Appointments}
  */
-const getAppointmentsForDay = (state, day) => {
-  const selectedDay = state.days.find(({ name }) => name === day);
+const getAppointmentsForDay = (state) => {
+  const selectedDay = state.days.find(({ name }) => name === state.day);
   return !selectedDay ? [] : getAppointmentsByIds(state.appointments, selectedDay.appointments);
 };
 
