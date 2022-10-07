07916727fda435921ab4442731efcabf09d8d249
NavyCuda <navysupra@gmail.com>
2022-10-06 19:21:18 -0700

    Add documentation

--- src/helpers/selectors/getAppointmentsForDay.js
+++ src/helpers/selectors/getAppointmentsForDay.js
@@ -1,7 +1,17 @@
 // src/helpers/selectors/getAppointmentsForDay.js
+
+/**
+ * 
+ * @param     {Object} state - Appointment state from which to query 
+ * @param     {Array.<{id: Number, name: String, spots: Number}>} state.days - Array of day objects
+ * @param     {Object} state.appointments
+ * @param     {String} day 
+ * @returns 
+ */
 const getAppointmentsForDay = (state, day) => {
   const selectedDay = state.days.find(({ name }) => name === day);
   return !selectedDay ? [] : getAppointmentsByIds(state.appointments, selectedDay.appointments);
 };
 
 export default getAppointmentsForDay;
+ge
