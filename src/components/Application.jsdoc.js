90113b6c7dc41124eebd22ae50b1bf6f7214c086
NavyCuda <navysupra@gmail.com>
2022-10-06 22:24:07 -0700

    Start jsdocs for application.js

---
+++ src/components/Application.jsdoc.js
@@ -0,0 +1,13 @@
+// src/components/Application.jsdoc.js
+// @ts-check
+/**
+ * Application module
+ * @module Application
+ */
+/**
+ * Application state
+ * @typedef     {Object}        State
+ * @property    {String}        day - The currently selected Day
+ * @property    {Array.<Day>}   days - Array of days to work with
+ * @property    {Appointments}  appointments - Object of Appointments
+ */
