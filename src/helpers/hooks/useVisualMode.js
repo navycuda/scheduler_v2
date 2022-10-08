7a63b1c40dc041692cf3f9154c399e0f6fbb4fdb
NavyCuda <navysupra@gmail.com>
2022-10-07 22:38:41 -0700

    Build out useVisualMode and functions

--- src/helpers/hooks/useVisualMode.js
+++ src/helpers/hooks/useVisualMode.js
@@ -1,12 +1,30 @@
 // src/helpers/hooks/useVisualMode.js
 // @ts-check
+import { useState } from "react";
 
 /**
  *
- * @param     {Object}    props
+ * @param     {String}    initial - The inital mode to set
  */
-const useVisualMode = (props) => {
-  return null;
+const useVisualMode = (initial) => {
+  const [ mode, setMode ] = useState(initial);
+
+  
+  /**
+   * Transition function, part of useVisualMode custom hook
+   * @param     {String}    nextMode - new visual mode to set
+   */
+  const transition = (nextMode) => {
+    setMode(nextMode);
+  };
+
+  /**
+   * Back function, part of useVisualMode custom hook, used to
+   * return to the previous view
+   */
+
+
+  return { mode, transition };
 };
 
 export default useVisualMode;
