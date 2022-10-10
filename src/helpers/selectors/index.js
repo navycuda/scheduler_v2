85afd2138229225dfe3c744b25d45cfb60b92749
NavyCuda <navysupra@gmail.com>
2022-10-10 16:02:46 -0700

    Add getSchedule and reorder

--- src/helpers/selectors/index.js
+++ src/helpers/selectors/index.js
@@ -1,5 +1,6 @@
 // src/helpers/selectors/index.js
-export {default as getAppointmentsForDay} from './getAppointmentsForDay';
-export {default as getAppointmentsByIds} from './getAppointmentsByIds';
+export {default as getSchedule} from './getSchedule'
 export {default as getInterview} from './getInterview';
 export {default as getAppointmentUrl} from './getAppointmentUrl';
+export {default as getAppointmentsByIds} from './getAppointmentsByIds';
+export {default as getAppointmentsForDay} from './getAppointmentsForDay';
