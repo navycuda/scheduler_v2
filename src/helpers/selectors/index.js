85ee64aee0cdf3f0fbe77ccbc9862615323168c9
NavyCuda <navysupra@gmail.com>
2022-10-10 15:44:38 -0700

    Export functions

--- src/helpers/selectors/index.js
+++ src/helpers/selectors/index.js
@@ -2,3 +2,4 @@
 export {default as getAppointmentsForDay} from './getAppointmentsForDay';
 export {default as getAppointmentsByIds} from './getAppointmentsByIds';
 export {default as getInterview} from './getInterview';
+export {default as getAppointmentUrl} from './getAppointmentUrl';
