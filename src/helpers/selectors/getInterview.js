c16cf986510b4495659d67b0065f343094b82066
NavyCuda <navysupra@gmail.com>
2022-10-11 10:06:08 -0700

    Fix bug

--- src/helpers/selectors/getInterview.js
+++ src/helpers/selectors/getInterview.js
@@ -11,7 +11,7 @@ import React from 'react';
 const getInterview = (state, interview) => {
   if (!interview) return null;
 
-  const interviewer = state.interviewers[interview.interviewer.id];
+  const interviewer = state.interviewers[interview.interviewer];
 
   return {
     interviewer,
