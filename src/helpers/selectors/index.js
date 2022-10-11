7a0400f514f06bdf4b16879319644da340c25a5c
NavyCuda <navysupra@gmail.com>
2022-10-11 11:05:27 -0700

    Remove unused reference

--- src/helpers/selectors/index.js
+++ src/helpers/selectors/index.js
@@ -1,5 +1,4 @@
 // src/helpers/selectors/index.js
-export {default as getSchedule} from './getSchedule'
 export {default as getInterview} from './getInterview';
 export {default as getAppointmentUrl} from './getAppointmentUrl';
 export {default as getAppointmentsByIds} from './getAppointmentsByIds';
