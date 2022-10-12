857890353f68c243635863f71c7a5f9e0bc5f5c7
NavyCuda <navysupra@gmail.com>
2022-10-12 13:34:16 -0700

    Consolidate Application

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,45 +1,17 @@
 // @ts-check
-
-import Axios from "axios";
 import React, { useState, useEffect } from "react";
 
 import "components/Application.scss";
 
 import DayList from "./DayList";
-import Appointment from "./Appointment";
-
-import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
 import { useApplicationData } from "helpers/hooks";
 
 const Application = () => {
   const {
     state,
     setDay,
-    bookInterview,
-    editInterview,
-    cancelInterview,
     getSchedule
   } = useApplicationData();
-  
-  const interviewers = getInterviewersForDay(state);
-  console.log('interviewers', interviewers);
-  const appointments = Object.values(getAppointmentsForDay(state)).map((appointment) => {
-    const interview = getInterview(state, appointment.interview);
-
-    return (
-      <Appointment 
-        key={appointment.id}
-        id={appointment.id}
-        time={appointment.time}
-        interview={interview}
-        interviewers={interviewers}
-        bookInterview={bookInterview}
-        editInterview={editInterview}
-        cancelInterview={cancelInterview}
-      />
-    );
-  });
-
 
   return (
     <main className="layout">
@@ -64,7 +36,7 @@ const Application = () => {
         />
       </section>
       <section className="schedule">
-        {appointments}
+        {getSchedule()}
       </section>
     </main>
   );
