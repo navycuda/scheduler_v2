d8147305692eb107c5c3b346ecfacdbd1ddbe6b6
NavyCuda <navysupra@gmail.com>
2022-10-12 17:33:32 -0700

    Flesh out the appointment data custom hook further

--- src/helpers/hooks/useApptFormData.js
+++ src/helpers/hooks/useApptFormData.js
@@ -1,9 +1,41 @@
 // src/helpers/hooks/useApptFormData.js
 // @ts-check
+import { useState } from "react";
 
 
+const emptyState = {
+  student: "",
+  interviewer: null
+};
+/**
+ * @type {UseApptFormData}
+ */
 const useApptFormData = () => {
+const [ state, setState ] = useState(emptyState);
 
+
+
+
+  const save = () => {
+
+  };
+
+  const reset = () => {
+
+  };
+
+  const input = () => {
+
+  };
+
+
+  return {
+    student:  state.student,
+    interviewer: state.interviewer,
+    save,
+    reset,
+    input
+  };
 };
 
 export default useApptFormData;
