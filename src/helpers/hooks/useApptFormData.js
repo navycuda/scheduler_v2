00efee6dee09151517391c03953e9dee20fa6078
NavyCuda <navysupra@gmail.com>
2022-10-13 11:16:56 -0700

    Return validation to function

--- src/helpers/hooks/useApptFormData.js
+++ src/helpers/hooks/useApptFormData.js
@@ -52,9 +52,9 @@ const [ state, setState ] = useState({...emptyState, ...initial});
    * @type {Save}
    */
   const save = () => {
-    // if (validate()) {
+    if (validate()) {
         state.onSave(state.student, state.interviewer);
-    // }
+    }
   };
 
   /**
