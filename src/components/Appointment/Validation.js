8ea0de88e61c66c1bd89a7b818a8dc00f33b151f
NavyCuda <navysupra@gmail.com>
2022-10-12 22:58:45 -0700

    Build validation component

--- src/components/Appointment/Validation.js
+++ src/components/Appointment/Validation.js
@@ -5,10 +5,18 @@ import React from 'react';
 /**
  * Use to display certain validation concerns
  * @param     {Object}    props
+ * @param     {String}    props.message The message to display during validation
  */
 const Validation = (props) => {
   return (
     <>
+      { props.message &&
+        <section
+          className="appointment__validation"
+        >
+          {props.message}
+        </section>
+      }
     </>
   );
 };
