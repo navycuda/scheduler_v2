493446647bbbe6174d3d17a8fa639b083e2d1e77
NavyCuda <navysupra@gmail.com>
2022-10-12 17:26:05 -0700

    Create the validation component

---
+++ src/components/Appointment/Validation.js
@@ -0,0 +1,16 @@
+// src/components/Appointment/Validation.js
+// @ts-check
+import React from 'react';
+
+/**
+ * Use to display certain validation concerns
+ * @param     {Object}    props
+ */
+const Validation = (props) => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Validation;
