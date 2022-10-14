2efc5eabfac72a294948e729100ce89be4a1b080
NavyCuda <navysupra@gmail.com>
2022-10-13 17:08:49 -0700

    Add another test from excerise

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -82,4 +82,19 @@ describe("Form", () => {
     /* 7. onSave is called with the correct arguments */
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
   });
+
+  it("submits the name entered by the user", () => {
+    const onSave = jest.fn();
+    const { getByText, getByPlaceholderText } = render(
+      <Form interviewers={interviewers} onSave={onSave} interviewer={1} />
+    );
+  
+    const input = getByPlaceholderText("Enter Student Name");
+  
+    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
+    fireEvent.click(getByText("Save"));
+  
+    expect(onSave).toHaveBeenCalledTimes(1);
+    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", 1);
+  });
 });
